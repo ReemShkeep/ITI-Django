a0be6df4b844b0dff9d4fd7ab8d842266f18f09b
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2023-04-23 17:29:29 +0200

    pushing URLs

--- eCommerceAPI/Smarket/market/urls.py
+++ eCommerceAPI/Smarket/market/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from market import views
+from .views import *
+#from .views import UserListCreateView, UserRetrieveUpdateDestroyView, CategoryListCreateView, CategoryRetrieveUpdateDestroyView, ProductListCreateView, ProductRetrieveUpdateDestroyView, OrderListCreateView, OrderRetrieveUpdateDestroyView, CheckoutListView, ProductSearch
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
+
 
 urlpatterns = [
     path('home/', views.home),
@@ -10,3 +14,19 @@ urlpatterns = [
     path('delete/<u_id>', views.deleteUser)
     
 ]
+
+
+
+"""from django.urls import path
+from market import views
+
+urlpatterns = [
+    path('home/', views.home),
+    path('user/<u_id>', views.getUser),
+    path('users/', views.getUsers),
+    path('new/', views.newUser),
+    path('edit/<u_id>', views.editUser),
+    path('delete/<u_id>', views.deleteUser)
+    
+]
+"""
